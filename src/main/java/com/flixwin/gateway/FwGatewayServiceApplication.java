1dd010bcd01f32b92fa8481a589f7fd59cba31c1
vikas <vikas@vikas-Vostro-3480>
2020-07-31 20:09:37 +0530

    gateway maven project

---
+++ src/main/java/com/flixwin/gateway/FwGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flixwin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwGatewayServiceApplication.class, args);
+	}
+
+}
